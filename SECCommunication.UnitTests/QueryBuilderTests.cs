be168825eb2188df410850c173af2af84ee02801
Nick Fisher <fisher.nick14@gmail.com>
2018-04-16 16:45:37 -0700

    Query Class started, unit tests introduced

---
+++ SECCommunication.UnitTests/QueryBuilderTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SECCommunication.UnitTests
+{
+    [TestFixture]
+    public class QueryBuilderTests
+    {
+        [Test]
+        public void AddQuery_AddsOne()
+        {
+            QueryBuilder q = new QueryBuilder();
+
+            q.AddQuery("action", "getcompany");
+
+            Assert.AreEqual(q.HTTPGetRequest, QueryBuilder.SECBaseRoute + "?action=getcompany");
+        }
+
+        [Test]
+        public void AddQuery_AddsThree()
+        {
+            QueryBuilder q = new QueryBuilder();
+
+            q.AddQuery("action", "getcompany");
+            q.AddQuery("otheraction", "getmorecompanies");
+            q.AddQuery("third", "three");
+
+            Assert.AreEqual(q.HTTPGetRequest, 
+                QueryBuilder.SECBaseRoute + "?action=getcompany&otheraction=getmorecompanies" +
+                "&third=three");
+        }
+
+
+        [Test]
+        public void RemoveQuery_RemovesOne()
+        {
+            QueryBuilder q = new QueryBuilder();
+            q.AddQuery("action", "getcompany");
+
+            q.RemoveQueryByKey("action");
+
+            Assert.AreEqual(QueryBuilder.SECBaseRoute, q.HTTPGetRequest);
+        }
+
+        [Test]
+        public void RemoveQuery_RemovesSome()
+        {
+            QueryBuilder q = new QueryBuilder();
+            q.AddQuery("action", "getcompany");
+            q.AddQuery("otheraction", "getmorecompanies");
+            q.AddQuery("third", "three");
+
+            q.RemoveQueryByKey("action");
+            q.RemoveQueryByKey("third");
+
+            Assert.AreEqual(q.HTTPGetRequest,
+                QueryBuilder.SECBaseRoute + "?otheraction=getmorecompanies");
+        }
+
+
+    }
+}
---
+++ SECCommunication/QueryBuilder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SECCommunication
+{
+    public class QueryBuilder
+    {
+        public const string SECBaseRoute = "https://www.sec.gov/cgi-bin/browse-edgar";
+
+        public string HTTPGetRequest { get
+            {
+                return BaseRoute + RequestString;
+            }
+        }
+
+        public string RequestString { get
+            {
+                return ConstructReqString();
+            } }
+
+        private string BaseRoute;
+        private Dictionary<string, string> KeyValuePairs = new Dictionary<string, string>();
+
+        public QueryBuilder(string baseRoute = SECBaseRoute)
+        {
+            BaseRoute = baseRoute;
+        }
+
+        public void AddQuery(string key, string value)
+        {
+            if (string.IsNullOrEmpty(key) || string.IsNullOrEmpty(value))
+                return;
+
+            if (KeyValuePairs.ContainsKey(key))
+                return;
+
+            KeyValuePairs.Add(key, value);
+        }
+
+        public void RemoveQueryByKey(string key)
+        {
+            KeyValuePairs.Remove(key);
+        }
+
+        private string ConstructReqString()
+        {
+            if (KeyValuePairs.Count > 0)
+            {
+                string req = "?";
+                int count = 0;
+                foreach (var pair in KeyValuePairs)
+                {
+                    req += $"{pair.Key}={pair.Value}";
+                    if (++count == KeyValuePairs.Count)
+                        return req;
+                    else
+                        req += "&";
+                }
+                return req;
+            }
+            else
+                return "";
+        }
+
+    }
+}
